170e05a5d4f3e5620e362c23a4df498dd60065c8
aleksandra <aleksandra.swigon91@gmail.com>
2023-08-01 19:41:29 +0200

    event detail

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,12 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
-  const param = useParams();
-  return (
-    <>
-      <h1>Event Detail Page</h1>
-      <p>Event ID: {param.eventId}</p>
-    </>
-  );
+  const data = useLoaderData();
+
+  return <EventItem event={data.event} />;
 };
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for the selected event." },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
+}
