c066d38af717ec1a8ec624e730dd7aed8a598f55
aleksandra <aleksandra.swigon91@gmail.com>
2023-08-01 19:04:19 +0200

    LoaderData

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,20 +1,23 @@
+import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  return (
-    <>
-      <EventsList />
-    </>
-  );
+  const data = useLoaderData();
+
+  const events = data.events;
+  console.log(events);
+
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
+
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
+    throw { message: "Could not fetch events" };
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
